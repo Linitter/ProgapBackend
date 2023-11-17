c462125aa0cda5370bc57f820a59cb6fc5d0d319
pedrovgc <pedrovgc@policiacivil.go.gov.br>
2023-11-17 09:43:12 -0300

    ajustes para rodar localhost e homologacao.

--- src/app.ts
+++ src/app.ts
@@ -9,11 +9,12 @@ const app = express();
 
 app.use((req, res, next) => {
   //Qual site tem permissão de realizar a conexão, no exemplo abaixo está o "*" indicando que qualquer site pode fazer a conexão
-    res.header("Access-Control-Allow-Origin", "*");
+  res.header('Access-Control-Allow-Origin', '*');
   //Quais são os métodos que a conexão pode realizar na API
+  // eslint-disable-next-line prettier/prettier
     res.header("Access-Control-Allow-Methods", 'GET,PUT,POST,DELETE');
-    app.use(cors());
-    next();
+  app.use(cors());
+  next();
 });
 
 app.use(cors({ credentials: true, origin: true }));
