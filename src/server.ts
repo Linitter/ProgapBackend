ec3993f4d379f965ab419dab4f037028f508de17
DITGO <citpcgo@gmail.com>
2023-11-29 09:04:58 -0300

    Atualização server.ts mundando a porta para 3000

--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,7 @@ import { app } from './app';
 import { APPDataSource } from './database/data-source';
 
 APPDataSource.initialize().then(() => {
-  app.listen(3010, () =>
+  app.listen(3000, () =>
     console.log(
       'Server is running!  🏆 Open http://localhost:3010 to see results',
     ),
